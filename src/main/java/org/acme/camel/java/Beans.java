0534c232f7be8b91e90a160e5f2685bb32926af8
Thomas Janssen <thomas.janssen@rubix.nl>
2019-07-15 20:50:02 +0200

    Camel ding

---
+++ src/main/java/org/acme/camel/java/Beans.java
@@ -0,0 +1,30 @@
+package org.acme.camel.java;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+
+import org.apache.camel.component.file.FileComponent;
+import org.apache.camel.component.timer.TimerComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class Beans {
+
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Produces
+    @Named("timer")
+    public TimerComponent timer() {
+        log.info("Creating timer component from CDI");
+        return new TimerComponent();
+    }
+
+    @Produces
+    @Named
+    public FileComponent fileComponent(){
+        return new FileComponent();
+    }
+
+}
---
+++ src/main/java/org/acme/camel/java/CamelRoute.java
@@ -0,0 +1,19 @@
+package org.acme.camel.java;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class CamelRoute extends RouteBuilder {
+
+    static final String I_AM_ALIVE_MESSAGE = "I'm alive !";
+
+    @Override
+    public void configure() {
+        from("timer:keep-alive?period={{camel.timer-route.period}}")
+                .id("timer-route-java")
+                .setBody(constant(I_AM_ALIVE_MESSAGE))
+                .log("${body}")
+                .to("log:keep-alive");
+
+    }
+
+}
